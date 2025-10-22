09567a68117f6c3290d9d0fd5fab7cd013e3862b
ThejokerDev <Ajimola63@gmail.com>
2025-10-22 06:25:09 +0000

    new

--- public/script.js
+++ public/script.js
@@ -71,3 +71,9 @@
   });
 
 })();
+
+
+
+ const toggleBtn = document.getElementById('toggleBtn');
+    const sidebar = document.getElementById('sidebar');
+    toggleBtn.addEventListener('click', () => sidebar.classList.toggle('open'));
